bc23a959c8045e409adb403663cd9515380c9df8
vakhabow <98717269+vakhabow@users.noreply.github.com>
2022-06-14 14:58:31 +0300

    Update index.js

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose');
 const express = require('express');
 const cors = require('cors');
 const path = require('path');
+const morgan = require('morgan');
 
 
 const app = express();
