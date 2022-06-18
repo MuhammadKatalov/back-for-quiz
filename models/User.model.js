297e2e46135c4d7e9341e561b3ca0bbf8721d762
Shamil <shamil>
2022-06-18 12:42:52 +0300

    регистрация почти готова

--- models/User.model.js
+++ models/User.model.js
@@ -3,12 +3,12 @@ const mongoose = require("mongoose");
 const userSchema = mongoose.Schema({
   login: {
     type: String,
-    required: true,
+    require: true,
     unique: true,
   },
   password: {
     type: String,
-    required: true,
+    require: true,
   },
 
   favoriteTest: [
